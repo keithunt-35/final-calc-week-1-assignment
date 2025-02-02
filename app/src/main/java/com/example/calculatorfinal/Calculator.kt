cab1e538d442bcdf55e5a88ab0e77a99873df22d
keithunt-35 <keithunt35@duck.com>
2025-02-02 23:35:17 +0300

    button colors

--- app/src/main/java/com/example/calculatorfinal/Calculator.kt
+++ app/src/main/java/com/example/calculatorfinal/Calculator.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -16,7 +17,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -72,9 +75,27 @@ fun CalculatorButton(btn: String){
     Box(modifier = Modifier.padding(8.dp)){
         FloatingActionButton(
             onClick = {/*todo*/},
+            modifier = Modifier.size(80.dp),
+            containerColor = getColor(btn),
+            contentColor = Color.White,
+
         ) {
-            Text(text = btn)
+            Text(
+                text = btn,
+                fontSize = 39.sp,
+                fontWeight = FontWeight.W200
+            )
         }
 
-        }
     }
+}
+fun getColor(btn: String): Color{
+    if (btn == "C" ||  btn == "AC")
+        return Color(0xFFF14949)
+    if (btn == "(" || btn == ")")
+        return Color(0xFF333A3F)
+    if (btn == "/" || btn == "*" || btn == "+" || btn == "-" || btn == "=")
+        return Color(0xFFFF9800)
+    return Color(0xFF4E6986)
+}
+
